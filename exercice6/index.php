bf180a4a98e376c60c6d6dbf44291836d37959cf
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 11:09:43 +0100

    Add files via upload

---
+++ exercice6/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 1</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 6</h1>
+    <?php 
+    
+    $name = 'Aloïs';
+    
+    echo 'Bonjour ' . $name . ', comment vas tu ?';
+    
+    ?>
+    
+    
+    
+</body>
+</html>
