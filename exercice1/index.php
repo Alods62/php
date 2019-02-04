b1108963838eddfccdce0d78ef2174597a8f1878
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 10:24:32 +0100

    Add files via upload

---
+++ exercice1/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 1</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 1</h1>
+    <?php
+    
+    $name = "toto"; // $ + chaine de caractère pour déclarer une variable en php, son type sera en fonction de sa valeur
+    echo $name; // echo pour afficher en php 
+    ?>  
+    
+    
+</body>
+</html>
