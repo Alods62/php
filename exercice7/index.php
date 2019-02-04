9c29d943e3710f90817ef1eb1d08fcee4d022af0
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 11:33:49 +0100

    Add files via upload

---
+++ exercice7/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 1</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 7</h1>
+    <?php 
+    
+    $lastname = "De Schepper";
+    $firstname = "Aloïs";
+    $age = 27;
+    
+    echo 'Bonjour ' . $lastname . ' ' . $firstname . ', tu as ' . $age . ' ans.'
+    
+    ?>
+    
+    
+    
+</body>
+</html>
