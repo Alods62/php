b8794ed6e9523d9c5816af88ae1a4ff619f8475a
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 11:34:48 +0100

    Add files via upload

---
+++ exercice8/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 1</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 8</h1>
+    <?php 
+    
+    $firstOperation = 3+4;
+    $secondOperation = 5*20;
+    $thirdOperation = 45 / 5;
+    
+    echo ' première opération = ' . $firstOperation . ', deuxième opération = ' . $secondOperation . ', troisième opération = '. $thirdOperation . '';
+    
+    ?>
+    
+    
+    
+</body>
+</html>
