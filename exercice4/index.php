af7440e215a77516e27e6b3dabe497e223f299d0
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 10:30:35 +0100

    Add files via upload

---
+++ exercice4/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 1</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 4</h1>
+    <?php 
+    
+    $string = "coucou";
+    $int = 5;
+    $float = 1.4;
+    $boolean = true;
+    echo "$string <br /> $int <br /> $float <br /> $boolean";
+    ?>
+    
+    
+    
+</body>
+</html>
