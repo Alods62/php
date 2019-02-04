9bb8b2e487fc2353a8b53e05b5cefa935b132133
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 10:29:17 +0100

    Add files via upload

---
+++ exercice3/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 1</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 3</h1>
+    <?php 
+    
+    $km = "1";
+    echo $km . 'km';
+    $km = "3";
+    echo $km . 'km';
+    $km = "125";
+    echo $km . 'km';
+    ?>
+    
+    
+    
+</body>
+</html>
